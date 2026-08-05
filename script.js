dfa0ef3f9d4ce6e88c8e81fd1bd8802f125853fc
nhatquang1505 <nhatquang150598@gmail.com>
2026-08-05 10:42:41 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // --- 1. CONFIGURATION & STATE ---
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbywy1_jjDMjGyKlUzAKS7Z09irHzuZWJqa81KKOfNIuRoESdVW0QqFVfSU35wdblX0TsA/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyYj4IcrfunOzONFAWd8pxTSgRWi57vFS_5bdSSwG0eftmBmpZkui-ibStXKOheM9fYIQ/exec';
 const WEB_APP_URL = SCRIPT_URL;
 const GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/1M6nnyKRVkTdafDdOm4w-UWnKQyvqt9qhDw13_g5TiDo/edit?usp=sharing";
 
